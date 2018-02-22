91060b121e775041de6ba5def7f56ea7bd0d1349
timattil <timattil@student.oulu.fi>
2018-02-22 11:56:33 +0200

    Add SQL commands for creation of tables game, player, ship, turn, shot

--- db/battleship_schema_dump.sql
+++ db/battleship_schema_dump.sql
@@ -1,10 +1,44 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS games(
-  game_id INTEGER PRIMARY KEY,
-  start DATE);
-CREATE TABLE IF NOT EXISTS players(
-  player_id CHAR PRIMARY KEY);
-
+CREATE TABLE IF NOT EXISTS game(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  start_time DATE,
+  end_time DATE,
+  x_size INTEGER,
+  y_size INTEGER,
+  turn_length INTEGER,
+  );
+CREATE TABLE IF NOT EXISTS player(
+  id INTEGER,
+  nickname TEXT,
+  game INTEGER,
+  PRIMARY KEY(id, game),
+  FOREIGN KEY(game) REFERENCES game(id) ON DELETE CASCADE);
+CREATE TABLE IF NOT EXISTS ship(
+   id INTEGER,
+   player INTEGER,
+   game INTEGER,
+   stern_x INTEGER,
+   stern_y INTEGER,
+   bow_x INTEGER,
+   bow_y INTEGER,
+   ship_type TEXT,
+   PRIMARY KEY(id, player, game),
+   FOREIGN KEY(player, game) REFERENCES player(id, game) ON DELETE CASCADE);
+CREATE TABLE IF NOT EXISTS turn(
+   turn_number INTEGER,
+   player INTEGER,
+   game INTEGER,
+   PRIMARY KEY(turn_number, player, game),
+   FOREIGN KEY(player, game) REFERENCES player(id, game) ON DELETE CASCADE);
+CREATE TABLE IF NOT EXISTS shot(
+   turn INTEGER,
+   player INTEGER,
+   game INTEGER,
+   x INTEGER,
+   y INTEGER,
+   shot_type TEXT,
+   PRIMARY KEY(turn, player, game, x, y),
+   FOREIGN KEY(turn, player, game) REFERENCES turn(turn_number, player, game) ON DELETE CASCADE);
 COMMIT;
 PRAGMA foreign_keys=ON;
