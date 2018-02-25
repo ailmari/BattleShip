8cbcc45a6849acab5628513cf4456d56476e3e80
TTO200120\artnieme <arttu.niemela@oulu.fi>
2018-02-25 12:30:03 +0200

    one data dump entry for each table

--- db/battleship_data_dump.sql
+++ db/battleship_data_dump.sql
@@ -1,2 +1,9 @@
-INSERT INTO "game" VALUES(1,"2018-2-21", NULL, 10, 10, NULL);
-INSERT INTO "player" VALUES(1, "testplayer1", 1);
+INSERT INTO "game" VALUES(12345,"2018-2-21", "2018-2-25", 10, 10, 5);
+
+INSERT INTO "player" VALUES(1919, "Fu1L_s41V0_n05CoP3_720", 12345);
+
+INSERT INTO "ship" VALUES(5454, 1919, 12345, 2, 3, 2, 6, "frigate");
+
+INSERT INTO "turn" VALUES(1, 1919, 12345);
+
+INSERT INTO "shot" VALUES(1, 1919, 12345, 4, 4, "single");
