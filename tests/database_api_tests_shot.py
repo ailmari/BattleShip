19552acacaa72e33eb7c051dc6c2138b73232b56
Timo Mattila <timattil@student.oulu.fi>
2018-02-25 19:21:04 +0200

    Add test files for all tables.

---
+++ tests/database_api_tests_shot.py
@@ -0,0 +1,76 @@
+'''
+Created on 25.02.2018
+
+Modified on 25.02.2018
+
+Tests for the API access to shot table.
+
+Programmable Web Project course work by:
+
+@author: arttu
+@author: niko
+@author: timo
+
+Based on course exercises code by:
+
+@author: ivan
+@author: mika oja
+'''
+
+
+import unittest
+import sqlite3
+
+from battleship import database
+
+
+ENGINE = database.Engine('db/battleship_test.db')
+
+
+class ShotDBTestCase(unittest.TestCase):
+    '''
+    Tests for methods that access the shot-table.
+    '''
+    @classmethod
+    def setUpClass(cls):
+        ''' Creates the database structure. Removes first any preexisting
+            database file
+        '''
+        print("Testing ", cls.__name__)
+        ENGINE.remove_database()
+        ENGINE.create_tables()
+
+    @classmethod
+    def tearDownClass(cls):
+        '''Remove the testing database'''
+        print("Testing ENDED for ", cls.__name__)
+        ENGINE.remove_database()
+
+    def setUp(self):
+        '''
+        Populates the database
+        '''
+        try:
+            ENGINE.populate_tables()
+            self.connection = ENGINE.connect()
+        except Exception as e:
+            print("error at setUp:", e)
+            ENGINE.clear()
+
+    def tearDown(self):
+        '''
+        Close underlying connection and remove all records from database
+        '''
+        self.connection.close()
+        ENGINE.clear()
+
+    def print_test_info(function):
+        def wrapped_function(self):
+            print('(' + function.__name__ + ')', function.__doc__)
+            function(self)
+        return wrapped_function
+
+
+if __name__ == "__main__":
+    print("Starting database shot tests...")
+    unittest.main()
